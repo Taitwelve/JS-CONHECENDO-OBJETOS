caeffb85371191014ac422d0bfd1486270d5c01a
Taitwelve <seuemail@exemplo.com>
2025-07-11 23:22:36 -0300

    fix: Conseguimos fazer alterações em objetos mesmo ele sendo declarado como const, não no nome do objeto, só nos dados que ele guarda

--- alterando-objetos.js
+++ alterando-objetos.js
@@ -11,7 +11,11 @@ console.log(estudante);
 estudante.idade = 31;
 console.log(estudante);
 
+const estudante2 = {}
+estudante2.nome = 'Maria da Silva';
+estudante2.idade = 28;
+console.log(estudante2);
 
 
 
-// Um objeot não é ordenado, então não podemos garantir a ordem de exibição das propriedades
+// Um objeto não é ordenado, então não podemos garantir a ordem de exibição das propriedades
