b233226bd5caafa8b0e8816fe84fdd087dfc1a5e
Taitwelve <seuemail@exemplo.com>
2025-07-11 22:53:53 -0300

    fix: exemplo de console.log com template string, chamado um valor expecífico pela sua chave. Key:Value

--- estrutura-objeto.js
+++ estrutura-objeto.js
@@ -7,5 +7,6 @@ const estudante = {
 
 console.log(estudante);
 console.log(estudante.nome);
+console.log(`O nome do estudante é ${estudante.nome} e ele tem ${estudante.idade} anos.`);
 
 //O objeto é sempre composto por chave:valor
