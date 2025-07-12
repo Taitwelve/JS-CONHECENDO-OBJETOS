6318bb5d32fc64e9353e96124e9c6f332a0a44c3
Taitwelve <seuemail@exemplo.com>
2025-07-11 23:12:29 -0300

    fix: resposta correta ✅

---
+++ exercicios.js
@@ -0,0 +1,9 @@
+const pet = {
+  nome: "Rex",
+  dataDeNascimento: "12/03/2017",
+  brinquedoFavorito: "bolinha",
+  cor: "cinza",
+};
+
+console.log(pet.dataDeNascimento);
+console.log(pet["brinquedoFavorito"]);
