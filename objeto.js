09fc13585b36fe53537399957a2f6e1833d80853
Taitwelve <seuemail@exemplo.com>
2025-07-11 22:42:39 -0300

    fix: criei arrays com informações pra ver o que consigo retirar delas

---
+++ objeto.js
@@ -0,0 +1,4 @@
+const listaCPFs = [ '11233655478', '11233655479', '11233655480', '11233655481', '11233655482' ];
+const infosPessoa = [ 'nome', 'José da Da Silva', 'idade', 30, 'altura', 1.75, 'peso', 70 ];
+
+console.log(infosPessoa[1]); 
