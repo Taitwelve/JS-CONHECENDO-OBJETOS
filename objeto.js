ba0328d8acf8e2776813a482989e4272e2c3acc5
Taitwelve <seuemail@exemplo.com>
2025-07-11 22:45:01 -0300

    fix: criando um exemplo de objeto

--- objeto.js
+++ objeto.js
@@ -2,3 +2,9 @@ const listaCPFs = [ '11233655478', '11233655479', '11233655480', '11233655481', 
 const infosPessoa = [ 'nome', 'José da Da Silva', 'idade', 30, 'altura', 1.75, 'peso', 70 ];
 
 console.log(infosPessoa[1]); 
+
+const objetoPessoa = {
+    nome: 'José da Da Silva',
+    idade: 30,
+    altura: 1.75,
+    peso: 70 }
