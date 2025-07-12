bd0eaff43d70c1397c033d491ef6cf5a9b77f947
Taitwelve <seuemail@exemplo.com>
2025-07-11 23:17:31 -0300

    fix: incluindo um novo dado no objeto estudante

---
+++ alterando-objetos.js
@@ -0,0 +1,8 @@
+const estudante = {
+    nome: 'José da Da Silva',
+    idade: 30,
+    cpf: "123.456.789-01",
+    turma: 'JS-2025'
+}
+estudante.estadoCivil = 'Solteiro'; 
+console.log(estudante.estadoCivil); 
