75af1a76770ea99b674aca6f1e68ebf976e0d5d7
Taitwelve <seuemail@exemplo.com>
2025-07-14 12:36:54 -0300

    fix: como acessar dados de uma mesma chave com vários valores, que ficam dentro de arrays

---
+++ dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: 'José da Da Silva',
+    idade: 30,
+    cpf: "123.456.789-01",
+    turma: 'JS-2025',
+    bolsista: true,
+    telefones: ["+55 27 99999-9999", "+55 27 88888-8888"],
+}
+
+console.log(estudante.telefones [0]);
