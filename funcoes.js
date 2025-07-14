9a5afb587d36792afe8439bd9cabb4a783abb801
Taitwelve <seuemail@exemplo.com>
2025-07-14 16:52:16 -0300

    fix: arrow function não consegue lidar com essa questão de contexto

--- funcoes.js
+++ funcoes.js
@@ -6,10 +6,7 @@ const estudante = {
     bolsista: true,
     telefones: ["+55 27 99999-9999", "+55 27 88888-8888"],
     media: 8.5,
-    estaAprovado: function(mediaBase) 
-    {
-        return this.media >= mediaBase ? true : false;
-    }
+    estaAprovado: (mediaBase) => this.media >= mediaBase ? true : false
 }
 
 console.log(estudante.estaAprovado(7.0)); 
