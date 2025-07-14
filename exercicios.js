16caaec401ed0e7107bb469ad87140285c7adb4f
Taitwelve <seuemail@exemplo.com>
2025-07-14 17:55:26 -0300

    fix: finalizado mais um exercício com sucesso

--- exercicios.js
+++ exercicios.js
@@ -114,3 +114,65 @@ console.log(pessoasSalvador);
 
 //____________________________________________________________________________________
 
+const calculadora = {
+    soma: function(a, b) {
+        return a + b;
+    },
+    subtracao: function(a, b) {
+        return a - b;
+    },
+    multiplicacao: function(a, b) {
+        return a * b;
+    },
+    divisao: function(a, b) {
+        if (b !== 0) {
+            return a / b;
+        } else {
+            return "Divisão por zero não é permitida.";
+        }
+    },
+    calcularMedia: function(numeros) {
+        const soma = numeros.reduce((total, numero) => total + numero, 0);
+        return soma / numeros.length;
+    }
+};
+
+console.log("Soma: " + calculadora.soma(5, 3));
+console.log("Subtração: " + calculadora.subtracao(8, 4));
+console.log("Multiplicação: " + calculadora.multiplicacao(6, 2));
+console.log("Divisão: " + calculadora.divisao(10, 2));
+console.log("Outra divisão: " + calculadora.divisao(8, 0));
+const numerosParaMedia = [10, 8, 6, 9, 7];
+console.log("Média: " + calculadora.calcularMedia(numerosParaMedia));
+
+//____________________________________________________________________________________
+
+const contaBancaria = {
+    titular: "João",
+    saldo: 1000,
+    depositar: function(valor) {
+        this.saldo += valor;
+    },
+    sacar: function(valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+        } else {
+            console.log("Saldo insuficiente para saque.");
+        }
+    }
+};
+
+const cliente = {
+    nome: "Carlos",
+    conta: contaBancaria
+};
+
+function mostrarSaldo(cliente) {
+    console.log(`Nome do cliente: ${cliente.nome}`);
+    console.log(`Saldo da conta: ${cliente.conta.saldo}`);
+}
+
+cliente.conta.depositar(500);
+cliente.conta.sacar(200);
+
+mostrarSaldo(cliente)
