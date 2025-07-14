8b0a3ba3dd8305b2fbb776a93d32d1aaa91e141b
Taitwelve <seuemail@exemplo.com>
2025-07-14 16:47:04 -0300

    fix: criando uma função dentro de um objeto

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+const estudante = {
+    nome: 'José da Da Silva',
+    idade: 30,
+    cpf: "123.456.789-01",
+    turma: 'JS-2025',
+    bolsista: true,
+    telefones: ["+55 27 99999-9999", "+55 27 88888-8888"],
+    media: 8.5,
+    estaAprovado: function(mediaBase) 
+    {
+        return this.media >= mediaBase ? true : false;
+    }
+}
+
+console.log(estudante.estaAprovado(7.0)); 
+
