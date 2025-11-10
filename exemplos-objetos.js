fdccb66272829e8002fe8946b355dbe5d0ca561c
Taitwelve <taynascimento15@gmail.com>
2025-11-10 15:11:22 -0300

    feat: add example of Object.keys() and Object.values() usage

---
+++ exemplos-objetos.js
@@ -0,0 +1,11 @@
+//Object.keys() e Object.values(): são usados para extrair informações específicas de um objeto. 
+// Esses métodos fornecem, respectivamente, as chaves e os valores presentes em um objeto. 
+// São úteis para iterar ou fazer operações específicas em conjuntos de dados de um objeto.
+const meuObjeto = { a: 1, b: 2, c: 3 };
+const chaves = Object.keys(meuObjeto);
+const valores = Object.values(meuObjeto);
+
+console.log(chaves); 
+console.log(valores); 
+
+
