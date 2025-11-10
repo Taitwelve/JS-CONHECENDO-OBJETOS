13425dc47e55338a003e22c90c1a02e20ba5c2eb
Taitwelve <taynascimento15@gmail.com>
2025-11-10 15:13:05 -0300

    feat: add example of Object.entries() to demonstrate key-value pairs extraction

--- exemplos-objetos.js
+++ exemplos-objetos.js
@@ -8,4 +8,11 @@ const valores = Object.values(meuObjeto);
 console.log(chaves); 
 console.log(valores); 
 
+//Object.entries(): este método retorna um array de arrays que representam pares chave-valor. 
+// É útil em situações que demandam iterações mais complexas ou manipulação mais minuciosa dos dados.
+
+const meusObjetos = { a: 1, b: 2, c: 3 };
+const entradas = Object.entries(meusObjetos);
+
+console.log(entradas);
 
